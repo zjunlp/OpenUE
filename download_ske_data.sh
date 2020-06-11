4d128648582efb16cf7f4350328ad2c23c233d49
zxlzr <zhangningyu16900@vip.qq.com>
2020-06-11 14:43:34 +0800

    fix

--- download_ske_data.sh
+++
@@ -1,4 +0,0 @@
-cd raw_data
-wget http://47.92.96.190/dataset/ske.tar.gz ske.tar.gz
-tar zxvf ske.tar.gz -C ske
-rm -f ske.tar.gz
--- serving_ske.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --name openue --network=host -p 8500:8500 -p 8501:8501 -v "$(pwd)/export/classification:/models/classification" -v "$(pwd)/export/seq:/models/seq"  -t tensorflow/serving

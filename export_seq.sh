01404c1ddc9fae2b9b0d722b506aa5e90336e629
zxlzr <zhangningyu16900@vip.qq.com>
2020-09-15 10:53:12 +0800

    add

--- export_seq.sh
+++ export_seq.sh
@@ -2,8 +2,8 @@ python export_sequnce_labeling.py \
   --do_predict=true \
   --task_name=sequence_labeling \
   --data_dir=openue/sequence_labeling/sequence_labeling_data \
-  --vocab_file=pretrained_model/chinese_L-12_H-768_A-12/vocab.txt \
-  --bert_config_file=pretrained_model/chinese_L-12_H-768_A-12/bert_config.json \
+  --vocab_file=pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/vocab.txt \
+  --bert_config_file=pretrained_model/chinese_wwm_ext_L-12_H-768_A-12/bert_config.json \
   --init_checkpoint=output/sequnce_labeling_model/epochs9/model.ckpt-85304 \
   --max_seq_length=128 \
   --output_dir=output/sequnce_labeling_model/wwm/ \

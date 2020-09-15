57ef3680bfab40104f61cc2cdd8017d8bef8d1c0
zxlzr <zhangningyu16900@vip.qq.com>
2020-09-15 10:45:27 +0800

    add download model and dataset script

---
+++ pretrained_model/download_bert_cn.sh
@@ -0,0 +1,3 @@
+mkdir chinese_wwm_ext_L-12_H-768_A-12
+wget http://47.92.96.190/model/chinese_wwm_ext_L-12_H-768_A-12.tar.gz
+tar -zxvf chinese_wwm_ext_L-12_H-768_A-12.tar.gz -C chinese_wwm_ext_L-12_H-768_A-12
---
+++ raw_data/download_ske_dataset.sh
@@ -0,0 +1,3 @@
+mkdir ske
+wget  http://47.92.96.190/dataset/ske.tar.gz 
+tar zxvf ske.tar.gz -C ske
